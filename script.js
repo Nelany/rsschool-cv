1fd95aa343487aa8592e1f74844ca6859e1c3083
Nelany <svetasweetka@gmail.com>
2023-07-08 23:07:34 +0200

    fix: fix js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const conteiner = document.querySelector(".feather-container")
 const feather =document.querySelector(".feather")
-const func = () => { eval(document.querySelector('.code__example').textContent); }
+const cast = () => { eval(document.querySelector('.code__example').textContent); }
 const container = document.querySelector('.feather-container');
-container.addEventListener('click', func);
-feather.addEventListener('click',func);
+container.addEventListener('click', cast);
